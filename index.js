6571674e4f853d8d34a22c5a98ef0a2562b211f3
Heinrich van Wyk <heinrichv@iosystems.co.za>
2022-02-25 21:02:31 +0200

    procfile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.post('/book', (req, res) => {
+app.get('/book', (req, res) => {
     res.send('success');
 });
 
