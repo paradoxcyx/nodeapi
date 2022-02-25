b91040c36846032854e9744eb962420d5e9c42f2
Heinrich van Wyk <heinrichv@iosystems.co.za>
2022-02-25 21:04:56 +0200

    second

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.get('/book', (req, res) => {
-    res.send('success');
-});
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+app.get('/', (req, res) => {
+    res
+      .status(200)
+      .send('Hello server is running')
+      .end();
+  });
+   
+  // Start the server
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+  });
